923292945577c85629b6094165134763f9b3b895
Aaron Maurer <ajmaurer89@gmail.com>
2014-12-05 15:00:06 -0600

    Started final for 343

---
+++ stat343/final.R
@@ -0,0 +1,22 @@
+library('parallel')
+library('alr3')
+library('faraway')
+library('car')
+library('MASS')
+library('locfit')
+library('xtable')
+library('nlme')
+library('quantreg')
+library('splines')
+library('rpart')
+library('randomForest')
+library('leaps')
+
+prob1<-T
+prob2<-F
+prob3<-F
+
+if (prob1) {
+    
+    
+}
