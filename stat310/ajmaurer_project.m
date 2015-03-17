23568e29773e26a895a2abcb59066e703a72a1c1
ajmaurer <ajmaurer89@gmail.com>
2015-03-17 16:18:41 -0500

    finished with project

--- stat310/ajmaurer_project.m
+++ stat310/ajmaurer_project.m
@@ -3,7 +3,7 @@
 
 %%% Caller
 setup = 1 
-prob = [0,0,1,0,0]
+prob = [0,0,0,0,1]
 
 %%% Setup
 if setup
@@ -31,7 +31,7 @@ if prob(1)
     l = cvx_optval
 
     % For values of tau going from 0 to 1 in increments of .01, round values 
-    % of y above tau to 0 and above tau to 1 to getxtau (I store all 100 vectors 
+    % of y above tau to 0 and above tau to 1 to get xtau (I store all 100 vectors 
     % in one matrix). Then, calculate objective value of c'x for each tau
     tau = .01*(1:100)                     % tau comes out as a 1x100 matrix
     % xtau(r,c) is 1 if y(r)>=tau(c), and otherwise 0
@@ -88,15 +88,21 @@ end
 if prob(3) 
 
     % Get Matricies.
-    newimage
+    newimage        % Given program
 
-    % Convert matricies so this can be formatted as a QP/LP. Matricies are huge but sparse
-    % both times, the objective is a norm of Cx
-    xs = sparse(reshape(S.*X,[m*n,1]))      % elements of X, or 0 if S is 0 (data missing)
+    % Convert matricies so this can be formatted as a QP/LP. Matricies huge but sparse
+    % For both problems, the objective is a norm of Cx
+    xs = sparse(reshape(S.*X,[m*n,1]))      % elements of X, or 0 if data missing
     SD = sparse(diag(reshape(S,[m*n,1])))   % has elements of S along diagonal
-    CT = speye([m*(n-1),m*n]) - [sparse(m*(n-1),m),speye(m*(n-1),m*(n-1))] % Top of C matrix
-    CB = speye([(m-1)*n,m*n]) - [sparse((m-1)*n,1),speye((m-1)*n,m*n-1)] % bottom of C matrix
+    CT = speye([m*(n-1),m*n]) - [sparse(m*(n-1),m),speye(m*(n-1),m*(n-1))] % Top of C
+    CB = speye([(m-1)*n,m*n]) - [sparse((m-1)*n,1),speye((m-1)*n,m*n-1)] % bottom C
     C  = [CT;CB]
+
+    % Show original image
+    colormap gray
+    imagesc(X)
+    print('image_2_orig.pdf')
+    close
 
     % Show sparse image
     colormap gray
@@ -106,7 +112,8 @@ if prob(3)
 
     %% part a: L2 variation. 
 
-    % optimize the problem, in this case a QP
+    % optimize the problem, in this case a QP since were minimizing the 2 norm
+    % SD*x == xs is asserting x_ij = a_ij when S=1, and is 0=0 when S=0 (always true)
     cvx_begin
         variable x(m*n)
         minimize(norm(C*x,2))
@@ -143,12 +150,13 @@ end
 if prob(4) 
 
     % get the data.
-    censoring
+    censoring           % given program
     AL = A(:,1:k)       % left 25 columns A
     AR = A(:,(k+1):n)   % remainder of A 
 
-    %% part a: fit a constrained QP
-    % Usual least squares for the first k data points, constraint that a'x>beta for remainder
+    %% part a: fit a constrained QP. 
+    % Usual least squares for the first k data points, 
+    % constraint that a'x>beta for remainder
     cvx_begin
         variable x_qp(d)
         minimize(norm(AL'*x_qp-b,2))
@@ -161,7 +169,7 @@ if prob(4)
 
     %% part c: comparison
     re_qp = norm(x_qp - x_true)/norm(x_true)
-    re_lp = norm(x_ls - x_true)/norm(x_true)
+    re_ls = norm(x_ls - x_true)/norm(x_true)
     
 end
 
@@ -169,7 +177,7 @@ end
 if prob(5)
 
     % get the data
-    sparsify
+    sparsify        % given program
     Ey = c'*mu      % expectation of c'y
     Vy = c'*Sigma*c % variance of c'y 
     cS = c'*Sigma   % when multiplied by x gives cov(c'a,y'a)
