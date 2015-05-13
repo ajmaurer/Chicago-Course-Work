c10255283ebe3323c4c8156c5895b4bf7e46f1ba
Aaron Maurer <ajmaurer89@gmail.com>
2015-05-13 02:30:55 -0500

    Finished problem 2, hw4

--- stat302/hw4.R
+++ stat302/hw4.R
@@ -8,25 +8,27 @@ library('reshape2')
 library('nlme')
 library('parallel')
 library('faraway')
+library("qvalue")
 
 ### Caller
-problem1 <- TRUE
+problem1 <- FALSE 
+problem2 <- TRUE
 
 ### Problem 1
-if (problem1){
-    ## ii - simulate simple hierarchical model
-    p.values <- function(x){
-        return(t.test(x)$p.value)
-    }
+## ii - simulate simple hierarchical model
+p.values <- function(x){
+    return(t.test(x)$p.value)
+}
 
-    simulateD <- function(pi0,sigb,n=10,m=1000,sigj=1){
-        output <- list()
-        output$beta     <- ifelse(rbinom(m,1,pi0),0,rnorm(m,0,sigb))
-        output$D        <- replicate(n,rnorm(m,output$beta,sigj))
-        output$p.values <- apply(output$D,1,p.values)
-        return(output)
-    }
+simulateD <- function(pi0,sigb,n=10,m=1000,sigj=1){
+    output <- list()
+    output$beta     <- ifelse(rbinom(m,1,pi0),0,rnorm(m,0,sigb))
+    output$D        <- replicate(n,rnorm(m,output$beta,sigj))
+    output$p.values <- apply(output$D,1,p.values)
+    return(output)
+}
    
+if (problem1){
     ## iii - generate simple p-values
     scenarios <- list(c(1,1),c(.5,3),c(0,3))    # each is a pi0, sigb pair
     simulations <- list()
@@ -39,24 +41,26 @@ if (problem1){
         hist(simulations[[i]]$p.values,breaks=20,freq=F,main=bquote("Histogram, "~pi[0] == .(pi0)~','~ sigma[b]== .(sigb)),xlab='p-value',ylab='Density')
         dev.off()
     }
+}
 
-    ## iv - implement Benjamini-Hochberg
-    BH <- function(pvals,alpha){
-        m    <- length(pvals)
-        ord  <- order(pvals)
-        test <- pvals[ord]< (1:m)*alpha/m
-        test[ord] <- (1:m)<Position(identity,test,right=T,nomatch=0)
-        return(test)
-    }
+## iv - implement Benjamini-Hochberg
+BH <- function(pvals,alpha){
+    m    <- length(pvals)
+    ord  <- order(pvals)
+    test <- pvals[ord]< (1:m)*alpha/m
+    test[ord] <- (1:m)<Position(identity,test,right=T,nomatch=0)
+    return(test)
+}
 
-    ## v - implement FDR
-    FDR <- function(beta,gamma,pFDR=FALSE){
-        R <- sum(gamma)
-        V <- sum(gamma & beta==0)
-        FDR <- ifelse(R>0,V/R,ifelse(pFDR,NA,0))
-        return(FDR)
-    }
+## v - implement FDR
+FDR <- function(beta,gamma,pFDR=FALSE){
+    R <- sum(gamma)
+    V <- sum(gamma & beta==0)
+    FDR <- ifelse(R>0,V/R,ifelse(pFDR,NA,0))
+    return(FDR)
+}
 
+if (problem1){
     ## vi - simulate actual FDR
     k <- 1000
     scenarios <- list(c(1,1),c(.5,3),c(0,3))    # each is a pi0, sigb pair
@@ -84,24 +88,92 @@ if (problem1){
             simulations.k[[i]]$pFDRavgs[j] <- mean(unlist(pFDRs),na.rm=T)       
         }
         # FDR
-        pdf(paste('hw4/pr1_vi_hist_',letters[i],'.pdf',sep=''))
+        pdf(paste('hw4/pr1_vi_',letters[i],'.pdf',sep=''))
         plot(alphas,simulations.k[[i]]$FDRavgs,type='b',main=bquote("Simulated FDR, "~pi[0] == .(pi0)~','~ sigma[b]== .(sigb)),xlab=expression(alpha),ylab='FDR',ylim=c(0,1))
         dev.off()
 
         # pFDR
-        pdf(paste('hw4/pr1_vii_hist_',letters[i],'.pdf',sep=''))
+        pdf(paste('hw4/pr1_vii_',letters[i],'.pdf',sep=''))
         plot(alphas,simulations.k[[i]]$pFDRavgs,type='b',main=bquote("Simulated pFDR, "~pi[0] == .(pi0)~','~ sigma[b]== .(sigb)),xlab=expression(alpha),ylab='pFDR',ylim=c(0,1))
         dev.off()
 
+
+    }
+}
+
+### Problem2 
+qgammas <- function(pvals,alpha){
+    qvals <- qvalue(pvals)$qvalues
+    return(qvals < alpha)
+}
+
+if (problem2){
+    part1 <- FALSE
+    part2 <- TRUE
+    if (part1) {
+        ## i - simulate actual FDR using qvals
+        k <- 1000
+        scenarios <- list(c(1,1),c(.5,3),c(0,3))    # each is a pi0, sigb pair
+        alphas    <- seq(.05,.5,.05)
+        FDRs      <- list()
+        pFDRs     <- list()
+        simulations.k <- list()
+        for (i in 1:3){
+            simulations.k[[i]] <-list()
+            pi0  <- scenarios[[i]][1]
+            sigb <- scenarios[[i]][2]
+            sims <- mclapply(1:k,function(x){simulateD(pi0,sigb)},mc.cores=4)
+            simulations.k[[i]]$p.values <- mclapply(sims,function(x){x$p.values},mc.cores=4)
+            simulations.k[[i]]$betas    <- mclapply(sims,function(x){x$beta}    ,mc.cores=4)
+            simulations.k[[i]]$gammas<-list()
+            simulations.k[[i]]$FDRs  <-NULL
+            simulations.k[[i]]$pFDRs <-NULL
+            for (j in 1:length(alphas)) {
+                simulations.k[[i]]$gammas[[j]] <- mclapply(simulations.k[[i]]$p.values,qgammas,alpha=alphas[j],mc.cores=4)
+                # FDR
+                FDRs <- mcmapply(FDR,simulations.k[[i]]$betas,simulations.k[[i]]$gammas[[j]],mc.cores=4)
+                simulations.k[[i]]$FDRavgs[j]  <- mean(unlist(FDRs))       
+                # pFDR
+                pFDRs <- mcmapply(FDR,simulations.k[[i]]$betas,simulations.k[[i]]$gammas[[j]],TRUE,mc.cores=4)
+                simulations.k[[i]]$pFDRavgs[j] <- mean(unlist(pFDRs),na.rm=T)       
+            }
+            # FDR
+            pdf(paste('hw4/pr2_i_FDR_',letters[i],'.pdf',sep=''))
+            plot(alphas,simulations.k[[i]]$FDRavgs,type='b',main=bquote("Simulated FDR using q values, "~pi[0] == .(pi0)~','~ sigma[b]== .(sigb)),xlab=expression(alpha),ylab='FDR',ylim=c(0,1))
+            dev.off()
+ 
+            # pFDR
+            pdf(paste('hw4/pr2_i_pFDR_',letters[i],'.pdf',sep=''))
+            plot(alphas,simulations.k[[i]]$pFDRavgs,type='b',main=bquote("Simulated pFDR using q values, "~pi[0] == .(pi0)~','~ sigma[b]== .(sigb)),xlab=expression(alpha),ylab='pFDR',ylim=c(0,1))
+            dev.off()
+ 
+        }
     }
 
+    if (part2) {
+        k <- 100
+        pi <- seq(0,1,length.out=9)
+        sig <- c(.5,1,2,4)
+        nicesig <-c('pt5','1','2','4')
+        outmat <- matrix(,nrow=length(sig),ncol=length(pi))
+        for (i in 1:length(sig)) {
+            for (j in 1:length(pi)) {
+                pi_gen <- function(x) {
+                    return(qvalue(simulateD(pi[j],sig[i])$p.values)$pi0)
+                }
+                pi0s <- mclapply(1:k,pi_gen,mc.cores=4)
+                outmat[i,j] <- mean(unlist(pi0s))
+            }
+            pdf(paste('hw4/pr2_ii_',nicesig[i],'.pdf',sep=''))
+            plot(pi,outmat[i,],type='b',main=bquote("True vs. Estimated "~ pi[0] ~', '~ sigma[b]== .(sig[i])),xlab=expression(pi[0]),ylab=expression(hat(pi)[0]),ylim=c(0,1),xlim=c(0,1))
+            abline(a=0,b=1,lty=2)
+            dev.off()
 
+        }
 
-            
+        #print(xtable(outmat),type="latex",file="hw4/pr2_ii.tex")
 
-
-
-
+    }
 
 }
 
