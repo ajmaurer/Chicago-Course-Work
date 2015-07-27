17f8cdfb7eb544bc48425fbc486a98c28328f481
Aaron Maurer <ajmaurer89@gmail.com>
2015-07-26 20:15:08 -0500

    Undoing stacking, because it didn't work

--- masters_paper/compare.py
+++ masters_paper/compare.py
@@ -78,7 +78,7 @@ def bern_y(X,p1,base_prob=.25,beta_sd=1):
 
 def generate(input):
     """ Testing function, all parameters are in a single tuple """
-    seed,gen,p0,p1,method,MCsize,stacks= input
+    seed,gen,p0,p1,method,MCsize= input
     npran.seed(seed)
     if gen.lower() == "ising":
         X    = ising_X(p1+p0,1000)
@@ -95,8 +95,7 @@ def generate(input):
                               knockoff='binary',
                               method=method,
                               MCsize=MCsize,
-                              intercept=True,
-                              stacks = stacks
+                              intercept=True
                               )
     bin_logit.fit()
     ori_logit = ko.knockoff_logit(ybin,X,.2,
@@ -120,8 +119,7 @@ def generate(input):
                               knockoff='binary',
                               method=method,
                               MCsize=MCsize,
-                              intercept=True,
-                              stacks = stacks
+                              intercept=True
                               )
     bin_lasso.fit(bin_logit.X_lrg)
     ori_lasso = ko.knockoff_lasso(ynor,X,.2,
@@ -139,7 +137,7 @@ def generate(input):
     with open('data/lasso_test_'+str(p0+p1)+'.txt','a') as f:
         f.write("%d\t%s\t%d\t%.5f\t%.5f\t%.5f\t%.5f\t%.5f\t%.5f\t%.5f\t%.5f\n" % (seed, gen, p1, bin_logit.M_distortion, np.mean(ko_corr), bin_FDR, bin_power, np.mean(ori_lasso.emp_ko_corr), ori_FDR, ori_power, corr))
 
-def batch_compare(b,p,p1s,gens,start_seed,method='fresh_sim',MCsize=100000,stacks=1,procs=4):
+def batch_compare(b,p,p1s,gens,start_seed,method='fresh_sim',MCsize=100000,procs=4):
     with open('data/backup_seeds.txt','r') as f:
         seeds = [int(seed) for seed in f.read().split()]
     inputs = []
@@ -147,7 +145,7 @@ def batch_compare(b,p,p1s,gens,start_seed,method='fresh_sim',MCsize=100000,stack
     for b in range(b):
         for p1 in p1s:
             for gen in gens:
-                inputs.append((seeds[i+start_seed],gen,p-p1,p1,method,MCsize,stacks))
+                inputs.append((seeds[i+start_seed],gen,p-p1,p1,method,MCsize))
                 i += 1
 
     pool = Pool(processes=procs)
